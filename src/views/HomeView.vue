8862c366aa36a07ee8be5531eb5500a0a8dc994f
Ttst <thibautstachnick@gmail.com>
2024-03-18 10:12:59 +0100

    add both chart to homeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <Pie />
+  <LineChart />
+
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
+import Pie from '@/components/Pie.vue';
+import LineChart from "@/components/LineChart.vue";
 @Options({
   components: {
-    HelloWorld,
+    Pie,
+    LineChart
   },
 })
 export default class HomeView extends Vue {}
