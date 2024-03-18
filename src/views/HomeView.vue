4c2b5f49274e70cdd09c2062d6969711f450fbc0
Ttst <thibautstachnick@gmail.com>
2024-03-18 11:15:43 +0100

    add written data components as example

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,18 @@
 <template>
   <Pie />
   <LineChart />
-
+  <WrittenData msg="Auberges actives" number-data="12 834"/>
+  <WrittenData msg="Utilisateurs actifs" number-data="8 027 809"/>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 import Pie from '@/components/Pie.vue';
 import LineChart from "@/components/LineChart.vue";
+import WrittenData from "@/components/WrittenData.vue";
 @Options({
   components: {
+    WrittenData,
     Pie,
     LineChart
   },
