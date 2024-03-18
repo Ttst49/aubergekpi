b74b5c8d4a34830af9f6c7fe73f65bb7043a1e2a
Ttst <thibautstachnick@gmail.com>
2024-03-18 11:15:21 +0100

    add written data component that get a message and a data to display in a simple way

---
+++ src/components/WrittenData.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+
+defineProps(['msg','numberData'])
+
+</script>
+
+<template>
+  <div class="data">
+    <h5 class="title">{{msg}}</h5>
+    <h1 class="number">{{numberData}}</h1>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+.data{
+  margin: 30px;
+}
+
+.title{
+  color: #b0b0b0;
+  margin: 0;
+}
+
+.number{
+  font-size: 3rem;
+  margin: 0;
+  padding: 0;
+}
+
+</style>
